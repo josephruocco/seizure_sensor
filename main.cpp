b3622323f79436f9102519c9b767437a94c05507
jcirce <53122804+jcirce@users.noreply.github.com>
2019-07-20 19:41:16 -0400

    final_version

--- main.cpp
+++ main.cpp
@@ -1,181 +1,189 @@
 #define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
 #include "PulseSensorPlayground.h"     // Includes the PulseSensorPlayground Library.
 
-//int t = analogRead(A5); // temperature
-int m = analogRead(A4); // myoware
-int h = analogRead(A3); // heart rate
-int X = analogRead(A0); 
-int Y = analogRead(A2);
-int Z = analogRead(A1);
+int m = analogRead(A4);     // myoware muscle sensor
+int h = analogRead(A3);     // heart rate
+int X = analogRead(A0);     // X-axis of Accelorometer
+int Y = analogRead(A2);     // Y-axis of Accelorometer
+int Z = analogRead(A1);     // Z-axis of Accelorometer
 
-bool data = false; //data collected post-seizure
 
-int jerks = -1;
-unsigned long jtime1 = 0; // store the initial time
-unsigned long jtime2; // store the current time
-unsigned long previousMillis; 
+
+unsigned long previousMillis;       
 unsigned long startSeizeMillis;
 unsigned long seizeDuration;
-bool currentJerk = false;
+
+bool stabilized = false;
 bool seizing = false;
+bool currentJerk = false; 
+bool data = false;                //data collected post-seizure
+bool tensionCalculated = false;   
+
+int jerks = -1;
 int prevBPM;
+int bpmCounter = 0;
 int bpmDiff; 
 int tension;
 int tensionAvg;
 int tensionTotal = 0;
-int minT = 645;          //for tension
-int maxT = 0;            //for tension
-int sample_count = 10;  //for tension
-bool tensionCalculated = false;
+int minT = 645;           //for tension
+int maxT = 0;             //for tension
+int sample_count = 10;    //for tension
+int manyJerks = 50;
+int fewJerks = 10;
+int highTension = 300;
 
-
-int mled = 47; // myoware
-int hled = 35; // heart rate
-int aled = 40; // accelorometer
+int mled = 47;            // myoware
+int hled = 35;            // heart rate
+int aled = 40;            // accelorometer
 
 const int mthresh = 200;  //muscle is tense
 const int hthresh = 550;  // heartbeat
-const int athresh = 150; // movement detected
+const int athresh = 150;  // movement detected
 
-PulseSensorPlayground pulseSensor;
+PulseSensorPlayground pulseSensor;  
 
 void setup() {
-  Serial.begin(9600); 
+   Serial.begin(9600); 
 
-  pulseSensor.analogInput(3);
-  pulseSensor.setThreshold(hthresh);
-
+   pulseSensor.analogInput(3);
+   pulseSensor.setThreshold(hthresh);
+  
   if (pulseSensor.begin()){
     Serial.print("working!");
     }
   
-
   pinMode(mled, OUTPUT);
   pinMode(hled, OUTPUT);
   pinMode(aled, OUTPUT);
 
 }
 
-int bpmCounter = 0;
-boolean stabilized = false;
-
 
 void loop() {
 
    
 
-int myBPM = pulseSensor.getBeatsPerMinute();// Calls function on our pulseSensor 
+    int myBPM = pulseSensor.getBeatsPerMinute();// Calls function on our pulseSensor 
 
   
-if (pulseSensor.sawStartOfBeat() && !stabilized) {            // Constantly test to see if "a beathappened".
- //Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
- //Serial.print("BPM: ");                        // Print phrase "BPM: "
-if (myBPM >= 60 && myBPM <= 100){
-  Serial.println("STABILIZED");
-  prevBPM = myBPM;
-  previousMillis = millis();
-  stabilized = true;
-}
- Serial.println(myBPM);
- // delay(20);                    // considered best practice in a simple sketch.// Print the value inside of myBPM.
+    if (pulseSensor.sawStartOfBeat() && !stabilized) {            // Constantly test to see if "a beathappened".
+ 
+    if (myBPM >= 60 && myBPM <= 100){
+        Serial.println("STABILIZED");
+        prevBPM = myBPM;
+        previousMillis = millis();
+        stabilized = true;
+        }
+        
+     Serial.println(myBPM);
+ 
 }
   
 //conditional runs when pulse sensor detects a stabilized heart beat  
 if (stabilized){
-
-  /*if (!pulseSensor.sawStartOfBeat()){
-    stabilized = false;
-    }*/
   
-//tracking seizure 
-unsigned long currentMillis = millis();
+    //tracking seizure 
+    unsigned long currentMillis = millis();
 
 
   
-//calculating BMP diff every 7 seconds
-if ( currentMillis - previousMillis >= 7000  && currentMillis - previousMillis <= 7015 && !seizing) {
-     bpmDiff = myBPM - prevBPM;
-     previousMillis = currentMillis;
+    //calculating BMP diff every 7 seconds
+    if ( currentMillis - previousMillis >= 7000  && currentMillis - previousMillis <= 7015 && !seizing) {
+        bpmDiff = myBPM - prevBPM;
+        previousMillis = currentMillis;
      
-    //seize threshold = 50+bmp jump
-  if (bpmDiff >= 50){
-    jerks = 0;
-    seizing = true;
-  }
+       //seize threshold = 50+bmp jump
+          if (bpmDiff >= 50){
+              jerks = 0;
+              seizing = true;
+              }
   
-    prevBPM = myBPM;
-    startSeizeMillis = currentMillis;
-}
+          prevBPM = myBPM;
+          startSeizeMillis = currentMillis;
+        }
 
-  //calculating seize duration
-  if (seizing){
-    data=false;
-    Serial.println("SEIZEING");
-    //Serial.print(myBPM);
-    Serial.println(analogRead(A4));
+      //calculating seizure duration
+      if (seizing){
+          data=false;
+          Serial.println("SEIZEING");
+  
+          //seize jerk threshold: 650 
+          if(analogRead(A0) >= 650 && !currentJerk) {
+             currentJerk = true;
+             jerks+=1; 
+            }
 
-
-    
-    //seize threshold: 650 
-    if(analogRead(A0) >= 650 && !currentJerk) {
-    currentJerk = true;
-    jerks+=1; 
-    //Serial.println(jerks);
-    }
-
-    //recording jerk  
-    if (currentJerk && analogRead(A0) < 650){
-      currentJerk = false;
-    }
+           //recording jerk  
+           if (currentJerk && analogRead(A0) < 650){
+             currentJerk = false;
+            }
     
     
-  //muscle sensor tracks tension
-  //collects sample_count number of tension values
-  //discards max and min values and stores average
+          //muscle sensor tracks tension
+          //collects sample_count number of tension values
+          //discards max and min values and stores average
    
-  if (!tensionCalculated){
-  for (int i=0; i < sample_count; i++){
-    tension = analogRead(A4);
-    if (tension > maxT){
-        maxT = tension;
-    }
-    if (tension < minT){
-      minT = tension;
-    }
-     tensionTotal += tension;
-  }
-    tensionTotal -= minT;
-    tensionTotal -= maxT;
-    tensionCalculated = true;
-  }
+          if (!tensionCalculated){
+              for (int i=0; i < sample_count; i++){
+                  tension = analogRead(A4);
+              if (tension > maxT){
+                  maxT = tension;
+               }
+              if (tension < minT){
+                  minT = tension;
+               }
+              tensionTotal += tension;
+            }
+           tensionTotal -= minT;
+           tensionTotal -= maxT;
+           tensionCalculated = true;
+         }
   
-    //stops seizing
-    if (myBPM <= 110){
-      seizeDuration = currentMillis - startSeizeMillis;
-     
-      Serial.print("STOPPED SEIZING");
-      seizing = false;
-    }
-  }
+        //stops seizing
+        if (myBPM <= 110){
+          seizeDuration = currentMillis - startSeizeMillis;
+         
+          Serial.print("STOPPED SEIZING");
+          seizing = false;
+         }
+       }
     
 
-  
-//printing number of jerks after a seizure had occured
-if (!seizing && jerks!= -1 && !data){
-  Serial.print("DATA: ");
-  Serial.print("SEIZE DURATION:");
-   Serial.println(seizeDuration);
-  Serial.print("JERKS: ");
-  Serial.println(jerks);
-  Serial.print("TENSION AVG: ");
-  tensionAvg = int(tensionTotal / (sample_count-2));
-  Serial.println(tensionAvg);
-  data = true; //prints once
-  tensionCalculated = false;
-}
-}
-}
+      //printing duration, number of jerks, and average tension after a seizure had occured
+      if (!seizing && jerks!= -1 && !data){
+        Serial.print("DATA: ");
+        Serial.print("SEIZE DURATION:");
+        Serial.println(seizeDuration);
+        Serial.print("JERKS: ");
+        Serial.println(jerks);
+        Serial.print("TENSION AVG: ");
+        tensionAvg = int(tensionTotal / (sample_count-2));
+        Serial.println(tensionAvg);
+        data = true;     //prints once
+        tensionCalculated = false;
 
+
+         if (jerks < manyJerks && tensionAvg > highTension){                           //low jerks, high tension
+              Serial.println("THIS SEIZURE WAS TONIC");
+          }
+         if (jerks > manyJerks && tensionAvg < highTension){                           //high jerks, low tension
+              Serial.println("THIS SEIZURE WAS CLONIC");
+          }
+         if (jerks < manyJerks && jerks > fewJerks && tensionAvg < highTension){      //some jerks, low tension
+              Serial.println("THIS SEIZURE WAS MYOCLONIC");
+          }
+         if (jerks < manyJerks && tensionAvg < highTension){                           //low jerks, low tension
+              Serial.println("THIS SEIZURE WAS ATONIC");
+          }
+         if (jerks > manyJerks && tensionAvg > highTension){                           //high jerks, high tension
+              Serial.println("THIS SEIZURE WAS TONIC-CLONIC");
+          } 
+      }
+    }
+
+ 
+  }
 
 /*
 
@@ -189,3 +197,4 @@ Tonic-clonic seizure: stiffness and repeated jerks
 
 Partial: elevated heart rate but symptoms are inconsistent
  */
+ 
